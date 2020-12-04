d26482fabdc783da97ebf0ad5c35ab83e094e290
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2020-12-04 15:58:48 -0800

    Create 1455A.cpp

---
+++ Codeforces/1455A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <cstdio>
+#include <cmath>
+#define pb push_back
+#define pf push_front
+#define ll long long
+#define mp make_pair
+#define REP(i,a) for(int i = 0; i < a; i++)
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define trav(a,x) for(auto& a: x)
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main(){
+  IO
+  int t;
+  cin >> t;
+  while(t--){
+    string s;
+    cin >> s;
+    cout << s.length() << endl;
+  }
+}
+ 
