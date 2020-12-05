b99b326d6bb57cf55352e2b3d145dd9d4547d737
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2020-12-04 16:00:42 -0800

    Create 1453A.cpp

---
+++ Codeforces/1453A.cpp
@@ -0,0 +1,48 @@
+/* Include all headers */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <cstdio>
+#include <cmath>
+#define pb push_back
+#define pf push_front
+#define ll long long
+#define mp make_pair
+#define REP(i,a) for(int i = 0; i < a; i++)
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define trav(a,x) for(auto& a: x)
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+void solve(){
+  int n,m;
+  cin >> n >> m;
+  set<int> s;
+  for(int i = 0; i < n + m; i++){
+    int x;
+    cin >> x;
+    s.insert(x);
+  }
+  cout << n + m - s.size() << endl;
+}
+int main(){
+  IO
+  int t;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
