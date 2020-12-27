bfe72997d38955ff6e7d2f669b03019d83488b5e
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2020-12-27 10:11:16 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,80 +1,86 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 class Problem3SquarePasture {
 public:
-    map<int,pair<int,int>> y;
-    int count(int x1, int x2, int y1, int y2){
-      if(x1 > x2){
-        swap(x1,x2);
+  static bool comp(pair<int, int> p1, pair<int,int> p2){
+    return p1.second < p2.second;
+  }
+  vector<pair<int,int>> v1;
+  bool inRange(int l, int r, int x){
+    return (x >= l && x <= r);
+  }
+  int contains(int y1){
+    int r = v1.size() - 1;
+    int l = 0;
+    while(r >= l){
+      int mid = (r + l)/2;
+      if(v1[mid].second == y1){
+        return mid;
       }
-      if(y1 > y2){
-        swap(y1,y2);
+      if(v1[mid].second < y1){
+        l = mid + 1;
       }
-      int ans = 0;
-      for(int i = y1; i <= y2; i++){
-        if(y[i].first >= x1 && y[i].first <= x2){
-          ans++;
-        }
+      if(v1[mid].second > y1){
+        r = mid - 1;
       }
-      return ans;
     }
-    int f(vector<pair<int,int>> v, int n, std::istream& in, std::ostream& out){
-      int counter = 0;
-      y.clear();
-      for(int i = 0; i < n; i++){
-        y[v[i].second] = v[i];
+    return -1;
+  }
+  int interval(int y1, int y2, int x1, int x2){
+    int counter = 0;
+    for(int i = 0; i < v1.size(); i++){
+      if(inRange(y1, y2, v1[i].second) && inRange(x1, x2, v1[i].first)){
+        counter++;
       }
-      for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-          //chose a rectangle with greater width than height
-          int width = abs(v[i].first - v[j].first);
-          int height = abs(v[i].second - v[j].second);
-          if(width <= height){
-            continue;
-          }
-          //width > height
-          int rectCount = count(v[i].first,v[j].first,v[i].second, v[j].second);
-          int x1 = min(v[i].first,v[j].first);
-          int x2 = max(v[i].first,v[j].first);
-          int y2 = max(v[i].second,v[j].second);
-          int y1 = min(v[i].second,v[j].second);
-          int w = width + 2;
-          int squareCount = count(x1,x2,y2,y2 - width);
-          counter++;
-          for(int lo = y2 - width + 1; lo <= y1; lo++){
-            int hi = lo + width;
-            //squareCount = num[hi] - num[lo - 1]
-            int numHigh = (y[hi].first >= x1 && y[hi].first <= x2);
-            int numLow = (y[lo - 1].first >= x1 && y[lo - 1].first <= x2);
-            squareCount += numHigh - numLow;
-            if(numHigh == 1 || numLow == 1){
-              //out << i << " " << j << endl;
-              counter++;
-            }
-          }
-        }
-      }
-      return counter;
     }
-	void solve(std::istream& in, std::ostream& out) {
-      int n;
-      in >> n;
-      vector<pair<int,int>> v;
-      for(int i = 0; i < n; i++){
-        int a, b;
-        in >> a >> b;
-        a++;
-        b++;
-        v.push_back({a,b});
+    return counter;
+  }
+  int ans(std::istream& in, std::ostream& out, int n, vector<pair<int,int>> v){
+    v1 = v;
+    sort(v1.begin(), v1.end(),comp);
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+      for(int j = i + 1; j < n; j++){
+        int x1 = min(v[i].first, v[j].first);
+        int x2 = max(v[i].first, v[j].first);
+        int y1 = min(v[i].second, v[j].second);
+        int y2 = max(v[i].second, v[j].second);
+        int width = x2 - x1;
+        int height = y2 - y1;
+        if(height >= width) {
+          continue;
+        }
+        //find out how much stuff is in the range [y2 - dx, y1]
+        //find out how much stuff is in the interval [y2, y1 + dx]
+        int a = interval(y2 - width, y1, x1, x2);
+        int b = interval(y2, y1 + width, x1, x2);
+        sum += a + b - 1;
+        out << i << " " << j << " " << a + b - 1 << endl;
       }
-      int x = f(v, n, in, out);
-      //out << x << endl;
-      for(int i = 0; i < n; i++){
-        swap(v[i].first, v[i].second);
-      }
-      int z = f(v, n, in, out);
-      out << x + z + n + 1 << endl;
-	}
+    }
+    return sum;
+  }
+  void solve(std::istream& in, std::ostream& out) {
+    int n;
+    in >> n;
+    vector<pair<int,int>> v;
+    for(int i = 0; i < n; i++){
+      int a, b;
+      in >> a >> b;
+      v.push_back(make_pair(a,b));
+    }
+    int x = ans(in, out, n, v);
+    v1 = v;
+    sort(v1.begin(), v1.end(),comp);
+    //out << x << endl;
+    for(int i = 0; i < n; i++) {
+      swap(v[i].first, v[i].second);
+    }
+    out << '-' << endl;
+    int y = ans(in, out, n, v);
+    out << y + x + n + 1 << endl;
+  }
 };
