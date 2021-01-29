7be707b4502bbac23639903a180ce4c6545e6e17
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2021-01-29 10:47:22 -0800

    Update asdfasd.cpp

--- USACO/asdfasd.cpp
+++ USACO/asdfasd.cpp
@@ -44,11 +44,11 @@ public:
       soFarMin[i] = INF;
     }
     for(int i = 0; i < N; i++){
+      int a;
+      if(i >= 1) a = soFarMin[i - 1];
+      else a = 0;
       for(int x = i + K - 1; x < min(i + 2 * K - 1,N); x++){
         for(int ch = 0; ch < M; ch++){
-          int a;
-          if(i >= 1) a = soFarMin[i - 1];
-          else a = 0;
           DP[ch][x] = min(DP[ch][x],interval(i,x,ch) + a);
           soFarMin[x] = min(soFarMin[x],DP[ch][x]);
         }
