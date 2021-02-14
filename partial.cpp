a9c543088bf9c143168d22b9c15c260d0a3a60ab
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2021-02-13 19:09:46 -0800

    Update partial.cpp

--- partial.cpp
+++ partial.cpp
@@ -18,9 +18,9 @@ bool operator<(const bessie a1, const bessie a2) {
   if(a1.x != a2.x){
     return a1.x < a2.x;
   }
-    vector<int> v1 = {a1.x, a1.y, a1.oranges, a1.dirx, a1.diry};
-    vector<int> v2 = {a2.x, a2.y, a2.oranges, a2.dirx, a2.diry};
-    return v1 < v2;
+  vector<int> v1 = {a1.x, a1.y, a1.oranges, a1.dirx, a1.diry};
+  vector<int> v2 = {a2.x, a2.y, a2.oranges, a2.dirx, a2.diry};
+  return v1 < v2;
 }
 class Problem3BessiesDream {
 public:
@@ -58,19 +58,34 @@ public:
         }
       }
     }
-    map<bessie, int> vis;
+    int vis[n][m][2][3][3];
+    for(int i = 0; i < n; i++){
+      for(int j= 0; j < m; j++){
+        for(int k = 0; k < 2; k++){
+          for(int l = 0; l < 3; l++){
+            for(int o = 0; o < 3; o++){
+              vis[i][j][k][l][o] = false;
+            }
+          }
+        }
+      }
+    }
     while (!q.empty()) {
       bessie cur = q.front().second;
+      //out << cur.x << " " << cur.y << endl;
       int dist = q.front().first;
       q.pop();
       if (cur.x == n - 1 && cur.y == m - 1) {
         out << dist << endl;
         return;
       }
-      if (cur.x < 0 || cur.y < 0 || cur.x >= n || cur.y >= m || vis[cur]) {
+      if (cur.x < 0 || cur.y < 0 || cur.x >= n || cur.y >= m) {
         continue;
       }
-      vis[cur] = true;
+      if(vis[cur.x][cur.y][cur.oranges][cur.dirx + 1][cur.diry + 1]){
+        continue;
+      }
+      vis[cur.x][cur.y][cur.oranges][cur.dirx + 1][cur.diry + 1] = true;
       if (adj[cur.x][cur.y] == 0) {
         continue;
       }else if (adj[cur.x][cur.y] == 1) {
