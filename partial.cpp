4dd91085235204c23fcfb66380ed0f76f7860fc2
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2021-02-13 16:54:30 -0800

    Create partial.cpp

---
+++ partial.cpp
@@ -0,0 +1,129 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+struct bessie {
+  int x;
+  int y;
+  bool oranges;
+  int dirx;
+  int diry;
+};
+int adj[1000][1000];
+bool operator<(const bessie a1, const bessie a2) {
+  if(a1.x != a2.x){
+    return a1.x < a2.x;
+  }
+  if(adj[a1.x][a2.x] == 4) {
+    vector<int> v1 = {a1.x, a1.y, a1.oranges, a1.dirx, a1.diry};
+    vector<int> v2 = {a2.x, a2.y, a2.oranges, a2.dirx, a2.diry};
+    return v1 < v2;
+  }
+  vector<int> v1 = {a1.x, a1.y, a1.oranges};
+  vector<int> v2 = {a2.x, a2.y, a2.oranges};
+  return v1 < v2;
+}
+class Problem3BessiesDream {
+public:
+  vector<pair<int, int>> d;
+  void solve(std::istream &in, std::ostream &out) {
+    d.push_back({0, 1});
+    d.push_back({1, 0});
+    d.push_back({-1, 0});
+    d.push_back({0, -1});
+    int n, m;
+    in >> n >> m;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        in >> adj[i][j];
+      }
+    }
+    queue<pair<int, bessie>> q;
+    if (adj[0][0] == 2) {
+      // if it's an orange tile
+      for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+          if (abs(i) == abs(j)) {
+            continue;
+          }
+          q.push({0, {0, 0, 1, i, j}});
+        }
+      }
+    } else {
+      for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+          if (abs(i) == abs(j)) {
+            continue;
+          }
+          q.push({0, {0, 0, 0, i, j}});
+        }
+      }
+    }
+    map<bessie, bool> vis;
+    while (!q.empty()) {
+      bessie cur = q.front().second;
+      int dist = q.front().first;
+      q.pop();
+      if (cur.x == n - 1 && cur.y == m - 1) {
+        out << dist << endl;
+        return;
+      }
+      if (cur.x < 0 || cur.y < 0 || cur.x >= n || cur.y >= m || vis[cur]) {
+        continue;
+      }
+      vis[cur] = true;
+      if (adj[cur.x][cur.y] == 0) {
+        continue;
+      }
+      if (adj[cur.x][cur.y] == 1) {
+        for (pair<int, int> p : d) {
+          int i = p.first;
+          int j = p.second;
+          q.push({dist + 1, {cur.x + i, cur.y + j, cur.oranges, i, j}});
+        }
+      }
+      if (adj[cur.x][cur.y] == 2) {
+        for (pair<int, int> p : d) {
+          int i = p.first;
+          int j = p.second;
+          q.push({dist + 1, {cur.x + i, cur.y + j, 1, i, j}});
+        }
+      }
+      if (adj[cur.x][cur.y] == 3) {
+        if (cur.oranges == 0) {
+          continue;
+        }
+        for (pair<int, int> p : d) {
+          int i = p.first;
+          int j = p.second;
+          q.push({dist + 1, {cur.x + i, cur.y + j, 1, i, j}});
+        }
+      }
+      if (adj[cur.x][cur.y] == 4) {
+        if(cur.x + cur.dirx < 0 || cur.x + cur.dirx >= n){
+          continue;
+        }
+        if(cur.y + cur.diry < 0 || cur.y + cur.diry >= m){
+          continue;
+        }
+        int nxt = adj[cur.x + cur.dirx][cur.y + cur.diry];
+        if(nxt == 3 || nxt == 0){
+          for (pair<int, int> p : d) {
+            int i = p.first;
+            int j = p.second;
+            q.push({dist + 1, {cur.x + i, cur.y + j, 0, i, j}});
+            continue;
+          }
+        }else {
+          q.push({dist + 1,
+                  {cur.x + cur.dirx, cur.y + cur.diry, 0, cur.dirx, cur.diry}});
+        }
+      }
+    }
+    out << -1 << endl;
+  }
+};
