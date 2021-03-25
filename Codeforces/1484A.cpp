6c6c4b9c350729abcab7d39db937b7e64b23be47
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2021-03-25 16:04:34 -0700

    Create 1484A.cpp

---
+++ Codeforces/1484A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void solve(){
+  int a, b;
+  cin >> a >> b;
+  cout << a * b << endl;
+}
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
