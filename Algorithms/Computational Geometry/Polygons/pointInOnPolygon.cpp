3ad1d5b561f5527703306f561343738ff6589363
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2021-04-05 17:23:53 -0700

    Update pointInOnPolygon.cpp

--- Algorithms/Computational Geometry/Polygons/pointInOnPolygon.cpp
+++ Algorithms/Computational Geometry/Polygons/pointInOnPolygon.cpp
@@ -8,6 +8,7 @@ struct point {
         return (p.x == x && p.y == y);
     }
 };
+
 struct triangle{
     point a;
     point b;
@@ -16,6 +17,7 @@ struct triangle{
         return (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
     }
 };
+
 bool innerPoint(point q, point a, point b){
     if(a.y > b.y) {
         swap(a, b);
@@ -23,15 +25,17 @@ bool innerPoint(point q, point a, point b){
     triangle t = {a,b,q};
     return (a.y <= q.y) && (b.y > q.y) && (t.signedArea() > 0);
 }
+
 bool between (point a, point b, point c){
     triangle t = {a,b,c};
-    if(t.signedArea() != 0){
+    if (t.signedArea() != 0){
         return false;
     }
     bool a1 = (b.x <= max(a.x, c.x) && b.y <= max(a.y, c.y));
     bool a2 = (b.x >= min(a.x, c.x) && b.y >= min(a.y, c.y));
     return a1 && a2;
 }
+
 struct polygon{
     vector<point> points;
     bool pointInPolygon(point &p){
@@ -41,13 +45,13 @@ struct polygon{
         for(int i = 0; i < n; i++){
             int cur = i;
             int nxt = (i + 1) % n;
-            if(points[cur].y == points[nxt].y){
+            if (points[cur].y == points[nxt].y){
                 continue;
             }
-            if(points[cur].y > points[nxt].y){
+            if (points[cur].y > points[nxt].y){
                 swap(cur, nxt);
             }
-            if(innerPoint(p,points[cur],points[nxt])){
+            if (innerPoint(p,points[cur],points[nxt])){
                 cntr++;
             }
         }
@@ -55,28 +59,29 @@ struct polygon{
     }
     bool pointOnPolygon(point &p){
         int n = points.size();
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++){
             int cur = i;
             int nxt = (i + 1) % n;
-            if(between(points[cur],p,points[nxt])){
+            if (between(points[cur],p,points[nxt])){
                 return true;
             }
         }
         return false;
     }
 };
+
 void solve(){
     int n;
     cin >> n;
     int m;
     cin >> m;
     polygon poly;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         point p;
         cin >> p.x >> p.y;
         poly.points.push_back(p);
     }
-    for(int i = 0; i < m; i++){
+    for (int i = 0; i < m; i++){
         point p;
         cin >> p.x >> p.y;
         if(poly.pointOnPolygon(p)){
@@ -90,6 +95,7 @@ void solve(){
         cout << "OUTSIDE" << endl;
     }
 }
+
 int main() {
     solve();
 }
