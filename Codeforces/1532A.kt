09ef5c7f69bf72f2b98f2cb968afd1cac1343582
MariaChrysafis <40708134+MariaChrysafis@users.noreply.github.com>
2021-06-22 12:21:25 -0700

    Create 1532A.kt

---
+++ Codeforces/1532A.kt
@@ -0,0 +1,13 @@
+import java.lang.AssertionError
+private fun readLn() = readLine()!!
+private fun readInt() = readLn().toInt()
+private fun readStrings() = readLn().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
+private fun readLongs() = readStrings().map { it.toLong() }
+fun main(args: Array<String>) {
+    val t = readLine()!!.toInt()
+    for (i in 0 until t) {
+        var (x, y) = readInts()
+        println(x + y);
+    }
+}
