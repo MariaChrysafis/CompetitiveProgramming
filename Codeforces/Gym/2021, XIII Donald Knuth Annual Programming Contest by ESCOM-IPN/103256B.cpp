4368691424f677664d8997e83f3d96439054594a
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-08-24 20:29:11 -0700

    Create 103256B.cpp

---
+++ Codeforces/Gym/2021, XIII Donald Knuth Annual Programming Contest by ESCOM-IPN/103256B.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#define ll long long
+using namespace std;
+void print(vector<int> v){
+    for(int i: v) cout << i << " ";
+    cout << endl;
+}
+int find_index(vector<int> v, int val){
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] == val){
+            return i;
+        }
+    }
+    return -1;
+}
+vector<int> reverse(vector<int> v, int pref){
+    cout << pref + 1 << " ";
+    reverse(v.begin(), v.begin() + pref + 1);
+    return v;
+}
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    int cntr = 0;
+    for(int i = 0; i < n; i += 2){
+        cntr += v[i + 1] - v[i];
+    }
+    cout << cntr << endl;
+}
+int main(){
+    solve();
+}
