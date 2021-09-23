8d6d892b80bc7371ac87d085b5b218584e266bae
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-09-23 13:45:53 -0700

    Create segTreeSimple.cpp

---
+++ Algorithms/Range Queries/segTreeSimple.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <map>
+
+#define ll long long
+using namespace std;
+
+struct segmentTree {
+    vector<ll> v;
+    vector<ll> val;
+
+    ll ID = 0;
+
+    ll op(ll a, ll b) {
+        return a + b;
+    }
+
+    ll query(int dum, int tl, int tr, int& l, int& r) {
+        if (tr < l || tl > r) {
+            return ID;
+        }
+        if (tl >= l && tr <= r) {
+            return val[dum];
+        }
+        ll mid = (tl + tr) >> 1;
+        dum = dum << 1;
+        return op(query(dum, tl, mid, l, r), query(dum + 1, mid + 1, tr, l, r));
+    }
+
+    ll query(int l, int r) {
+        return query(1, 0, (int)v.size() - 1, l, r);
+    }
+
+    void update(int x, ll y) {
+        int cur = (int) v.size() + x;
+        int curX = x;
+        int curY = x;
+        while (true) {
+            val[cur] = op(op(query(curX, x - 1), query(x + 1, curY)), y);
+            if (cur == 0) {
+                break;
+            }
+            if(cur % 2 == 0) {
+                curY = 2 * curY - curX + 1;
+            } else {
+                curX = 2 * curX - curY - 1;
+            }
+            cur /= 2;
+        }
+    }
+
+    void resz(int n) {
+        v.resize((1 << (int) ceil(log2(n))));
+        val.resize(v.size() * 2);
+    }
+
+};
+
+void solve() {
+    segmentTree st;
+    int n, q;
+    cin >> n >> q;
+    st.resz(n);
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        st.update(i, x);
+    }
+    while (q--) {
+        int t, x, y;
+        cin >> t >> x >> y;
+        if (t == 1) {
+            st.update(x, y);
+        } else {
+            cout << st.query(x, y - 1) << '\n';
+        }
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+}
+
