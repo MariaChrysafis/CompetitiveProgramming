9e571ef0539adac65c6516d2680a7c7cd7426043
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-09-23 14:11:14 -0700

    Create A.cpp

---
+++ Codeforces EDU/Segment Tree, Part 2/Step 1/A.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <map>
+
+#define ll long long
+using namespace std;
+
+struct segmentTree {
+    vector<ll> v;
+    vector<ll> vec;
+    vector<ll> addLater;
+
+    void upd(int dum, int tl, int tr, int &l, int &r, ll val) {
+        if (tr < l || tl > r) {
+            return;
+        }
+        if (tl >= l && tr <= r) {
+            addLater[dum] += val;
+            return;
+        }
+        ll mid = (tl + tr) >> 1;
+        dum = dum << 1;
+        upd(dum, tl, mid, l, r, val);
+        upd(dum + 1, mid + 1, tr, l, r, val);
+    }
+
+    void upd(int l, int r, int val) {
+        upd(1, 0, v.size() - 1, l, r, val);
+    }
+
+    ll get(int x) {
+        int cur = x + v.size();
+        ll ans = 0;
+        while (true) {
+            ans += addLater[cur];
+            if (cur == 0) {
+                break;
+            }
+            cur /= 2;
+        }
+        return ans;
+    }
+
+    void resz(int n) {
+        v.resize((1 << (int) ceil(log2(n))));
+        vec.resize(v.size() * 2);
+        addLater.resize(v.size() * 2);
+    }
+
+};
+
+void solve() {
+    segmentTree st;
+    int n, q;
+    cin >> n >> q;
+    st.resz(n);
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            ll x, y, v;
+            cin >> x >> y >> v;
+            st.upd(x, y - 1, v);
+        } else {
+            int x;
+            cin >> x;
+            cout << st.get(x) << '\n';
+        }
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+}
