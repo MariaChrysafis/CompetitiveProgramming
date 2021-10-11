b6a2a6f6bcaf0b94d0a5c03d49cd0caf1f0b76cd
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-10-10 18:58:39 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <iostream>
 #include <queue>
+
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
 
@@ -12,20 +13,25 @@ struct Node {
     int y;
     int dist;
 };
+
+struct nodeComparator {
+    bool operator()(Node const &p1, Node const &p2) {
+        return p1.dist < p2.dist;
+    }
+};
+
 vector<string> arr;
 vector<vector<int>> nearestRock;
-int main(){
-    //freopen("hamming.in", "r", stdin);
+
+int main() {
     int n, d;
     cin >> n >> d;
     queue<Node> q;
-    //cout << "YES\n";
     for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
         arr.push_back(s);
     }
-    //cout << "YES\n";
     nearestRock.resize(n);
     for (int i = 0; i < n; i++) {
         nearestRock[i].resize(n);
@@ -40,9 +46,8 @@ int main(){
     }
     bool hv[n][n];
     for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) hv[i][j] = false;
-    while(!q.empty()) {
+    while (!q.empty()) {
         Node cur = q.front();
-        //cout << cur.x << " " << cur.y << endl;
         q.pop();
         if (cur.dist > nearestRock[cur.x][cur.y] || hv[cur.x][cur.y]) {
             continue;
@@ -51,7 +56,7 @@ int main(){
         hv[cur.x][cur.y] = true;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if(abs(i) + abs(j) == 1) {
+                if (abs(i) + abs(j) == 1) {
                     Node nxt;
                     nxt.dist = cur.dist + 1;
                     nxt.x = i + cur.x;
@@ -63,34 +68,29 @@ int main(){
             }
         }
     }
-    //return 0;
-    queue <Node> mq;
+    queue<Node> pq;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             Node b;
             b.x = i, b.y = j, b.dist = 0;
-            if(arr[i][j] == 'S') {
-                mq.push(b);
+            if (arr[i][j] == 'S') {
+                pq.push(b);
             }
         }
     }
-    int nearestSource[n][n];
+    vector<vector<int>> nearestSource(n), mm(n);
+    bool hasVisited[n][n];
     for (int i = 0; i < n; i++) {
+        nearestSource[i].resize(n), mm[i].resize(n);
         for (int j = 0; j < n; j++) {
             nearestSource[i][j] = -1;
-        }
-    }
-    bool hasVisited[n][n];
-    int mm[n][n];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
             hasVisited[i][j] = false;
             mm[i][j] = -1;
         }
     }
-    while(!mq.empty()) {
-        Node cur = mq.front();
-        mq.pop();
+    while (!pq.empty()) {
+        Node cur = pq.front();
+        pq.pop();
         if (cur.x < 0 || cur.y < 0 || cur.x >= n || cur.y >= n || hasVisited[cur.x][cur.y]) {
             continue;
         }
@@ -105,15 +105,15 @@ int main(){
                     if (nearestSource[nxt.x][nxt.y] != -1) {
                         continue;
                     }
-                    if ((nxt.dist - 1)/d >= nearestRock[nxt.x][nxt.y]) {
+                    if ((nxt.dist - 1) / d >= nearestRock[nxt.x][nxt.y]) {
                         continue;
                     }
-                    if(nxt.dist/d >= nearestRock[nxt.x][nxt.y]) {
+                    if (nxt.dist / d >= nearestRock[nxt.x][nxt.y]) {
                         mm[nxt.x][nxt.y] = nxt.dist - 1;
                         continue;
                     }
                     if (hasVisited[nxt.x][nxt.y]) continue;
-                    mq.push(nxt);
+                    pq.push(nxt);
                 }
             }
         }
@@ -126,36 +126,35 @@ int main(){
             }
         }
     }
+    priority_queue<Node, vector<Node>, nodeComparator> mq;
     mq = {};
-    vector<pair<int, pair<int,int>>> vec;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             valid[i][j] = false;
-            if(mm[i][j] != -1) {
+            if (mm[i][j] != -1) {
                 Node n1;
                 n1.x = i, n1.y = j, n1.dist = mm[i][j];
                 mq.push(n1);
             }
         }
-        //cout << endl;
     }
-    while(!mq.empty()) {
-        Node cur = mq.front();
+    while (!mq.empty()) {
+        Node cur = mq.top();
         mq.pop();
+        if (valid[cur.x][cur.y]) {
+            continue;
+        }
         valid[cur.x][cur.y] = true;
         if (cur.dist == 0) {
             continue;
         }
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
-                if(abs(dx) + abs(dy) == 1) {
-                    //cout << dx << " " << dy << endl;
+                if (abs(dx) + abs(dy) == 1) {
                     Node n1 = cur;
                     n1.x += dx, n1.y += dy;
                     n1.dist--;
                     mq.push(n1);
-                    //cout << n1.x << " " << n1.y << " " << n1.dist << endl;
-                    //valid[n1.x][n1.y] = true;
                 }
             }
         }
@@ -163,10 +162,8 @@ int main(){
     int cntr = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            //cout << valid[i][j] << " ";
             cntr += valid[i][j];
         }
-        //cout << endl;
     }
     cout << cntr << endl;
 
