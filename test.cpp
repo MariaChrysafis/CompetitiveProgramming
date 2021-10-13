59564f0788332556202fdf6d2c85a1b1b03dce72
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-10-13 11:52:35 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -23,14 +23,24 @@ int memoize(int i, int j) {
     marked[i][j] = true;
     return (dp[i][j] = ans);
 }
+//const int INF = (int)2e6 + 3;
 int main() {
     freopen("threesum.in", "r", stdin);
     freopen("threesum.out", "w", stdout);
     int n, q;
     cin >> n >> q;
     vector<int> v(n);
+    int oc[n][n + 1];
     for (int i = 0; i < n; i++) {
         cin >> v[i];
+        //oc[v[i]]++;
+    }
+    for (int i = 0; i < n; i++) {
+        //how many are v[i] up to jth point
+        oc[i][0] = 0;
+        for (int j = 1; j <= n; j++) {
+            oc[i][j] = oc[i][j - 1] + (v[j - 1] == v[i]);
+        }
     }
     dp.resize(n), nums.resize(n), marked.resize(n);
     for (int i = 0; i < n; i++) {
@@ -42,14 +52,18 @@ int main() {
     }
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n; j++) {
+            int fnd = -1;
             for (int k = i + 1; k <= j - 1; k++) {
                 if (v[i] + v[k] + v[j] == 0) {
-                    nums[i][j]++;
+                    //nums[i][j]++;
+                    fnd = k;
+                    break;
                 }
             }
+            if (fnd == -1) continue;
+            nums[i][j] = oc[fnd][j] - oc[fnd][i + 1];
         }
     }
-    //cout << nums[1][3] << endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             dp[i][j] = memoize(i, j);
@@ -58,8 +72,6 @@ int main() {
     while (q--) {
         int x, y;
         cin >> x >> y;
-        cout << dp[x - 1][y - 1] << endl;
+        cout << dp[x - 1][y - 1] << '\n';
     }
 }
-
-
