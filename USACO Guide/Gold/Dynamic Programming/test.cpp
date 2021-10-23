cf2d70392e2b6e11ac10a148af094160e9396d0a
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-10-22 18:27:16 -0700

    Update test.cpp

--- USACO Guide/Gold/Dynamic Programming/test.cpp
+++ USACO Guide/Gold/Dynamic Programming/test.cpp
@@ -16,8 +16,8 @@ ll interval (int color, int l, int r) {
     return cost[color][r + 1] - cost[color][l];
 }
 int main() {
-    //freopen("cowmbat.in", "r", stdin);
-    //freopen("cowmbat.out", "w", stdout);
+    freopen("cowmbat.in", "r", stdin);
+    freopen("cowmbat.out", "w", stdout);
     int N, M, K;
     cin >> N >> M >> K;
     string s;
@@ -40,27 +40,35 @@ int main() {
     for (int i = 0; i < M; i++) {
         cost[i] = {0};
         for (int j = 1; j <= N; j++) {
-            cost[i].push_back(cost[i].back() + adj[i][s[j - 1] - 'a']);
+            cost[i].push_back(cost[i].back() + adj[s[j - 1] - 'a'][i]);
         }
     }
-    ll dp[M][N + 1];
+    ll dp[M][N];
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
+            dp[i][j] = (int)1e9;
+        }
+    }
+    for (int i = 0; i < M; i++) {
+        for (int j = K - 1; j < N; j++) {
             dp[i][j] = interval(i, 0, j);
         }
     }
     for (int i = 0; i < N; i++) {
         for (int col1 = 0; col1 < M; col1++) {
-            for (int l = K; l <= i - 1; l++) {
+            for (int l = K; l <= N; l++) {
                 for (int col2 = 0; col2 < M; col2++) {
+                    if (i - l < 0) continue;
                     dp[col1][i] = min(dp[col1][i], dp[col2][i - l] + interval(col1, i - l + 1, i));
                 }
             }
         }
     }
+    //cout << dp[3][K - 1] << endl;
     ll myMin = INT_MAX;
     for (int i = 0; i < M; i++) {
         myMin = min(myMin, dp[i][N - 1]);
+        //cout << i << " "<< dp[i][N - 1] << endl;
     }
     cout << myMin;
 }
