9c823f086f6d84eef64e08a02ce696cbb1e6c9a2
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-10-22 18:40:29 -0700

    Update test.cpp

--- USACO Guide/Gold/Dynamic Programming/test.cpp
+++ USACO Guide/Gold/Dynamic Programming/test.cpp
@@ -66,8 +66,8 @@ struct segmentTree {
 
 };
 int main() {
-    freopen("cowmbat.in", "r", stdin);
-    freopen("cowmbat.out", "w", stdout);
+    //freopen("cowmbat.in", "r", stdin);
+    //freopen("cowmbat.out", "w", stdout);
     segmentTree st[26];
     int N, M, K;
     cin >> N >> M >> K;
@@ -110,11 +110,14 @@ int main() {
     }
     ll curMin[N];
     for (int i = 0; i < N; i++) {
+        curMin[i] = LLONG_MAX;
+    }
+    for (int i = 0; i < N; i++) {
         for (int col1 = 0; col1 < M; col1++) {
             for (int l = K; l <= i; l++) {
-                for (int col2 = 0; col2 < M; col2++) {
-                    dp[col1][i] = min(dp[col1][i], dp[col2][i - l] - cost[col1][i - l + 1] + cost[col1][i + 1]);
-                }
+                //for (int col2 = 0; col2 < M; col2++) {
+                    dp[col1][i] = min(dp[col1][i], curMin[i - l] - cost[col1][i - l + 1] + cost[col1][i + 1]);
+                //}
             }
             curMin[i] = min(curMin[i], dp[col1][i]);
         }
