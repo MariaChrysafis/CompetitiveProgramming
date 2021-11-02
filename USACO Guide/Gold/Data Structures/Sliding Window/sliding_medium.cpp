c91e1f187f22c34c17861686af3f32bec48927a5
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-11-01 18:39:29 -0700

    Create sliding_medium.cpp

---
+++ USACO Guide/Gold/Data Structures/Sliding Window/sliding_medium.cpp
@@ -0,0 +1,103 @@
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <set>
+#include <map>
+ 
+#define ll int
+using namespace std;
+struct segmentTree {
+    vector<ll> v;
+    vector<ll> val;
+ 
+    ll ID = 0;
+ 
+    ll query(int dum, int tl, int tr, int& l, int& r) {
+        if (tr < l || tl > r) {
+            return ID;
+        }
+        if (tl >= l && tr <= r) {
+            return val[dum];
+        }
+        ll mid = (tl + tr) >> 1;
+        dum = dum << 1;
+        return query(dum, tl, mid, l, r) + query(dum + 1, mid + 1, tr, l, r);
+    }
+ 
+    ll query(int l, int r) {
+        return query(1, 0, (int)v.size() - 1, l, r);
+    }
+ 
+    void update(int x, ll y) {
+        int cur = (int) v.size() + x;
+        int curX = x;
+        int curY = x;
+        while (true) {
+            val[cur] += y;
+            if (cur == 0) {
+                break;
+            }
+            if(cur % 2 == 0) {
+                curY = 2 * curY - curX + 1;
+            } else {
+                curX = 2 * curX - curY - 1;
+            }
+            cur /= 2;
+        }
+    }
+ 
+    void resz(int n) {
+        v.resize((1 << (int) ceil(log2(n))));
+        val.resize(v.size() * 2);
+    }
+ 
+};
+segmentTree st;
+int binSearch (int l, int r, int ind) {
+    int m = (l + r )/2;
+    if (l == r) {
+        return l;
+    }
+    if (st.query(0, m) >= ind + 1) {
+        return binSearch(l, m, ind);
+    } else {
+        return binSearch(m + 1, r, ind);
+    }
+}
+int median (int k, int n) {
+    return binSearch(0, n + 1, (k - 1)/2);
+}
+int main() {
+    //freopen("shortcut.in", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    st.resz(n + 5);
+    vector<int> arr(n);
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        s.insert(arr[i]);
+    }
+    map<int,int> myMap;
+    int rev[n + 1];
+    int cntr = 0;
+    for (int i: s) {
+        cntr++;
+        myMap[i] = cntr;
+        rev[cntr] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        arr[i] = myMap[arr[i]];
+    }
+    for (int i = 0; i < k; i++) {
+        st.update(arr[i], 1);
+    }
+    for (int i = 0; i <= n - k; i++) {
+        cout << rev[median(k, n)] << " ";
+        if (i != n - k) st.update(arr[i + k], 1);
+        st.update(arr[i], - 1);
+    }
+}
