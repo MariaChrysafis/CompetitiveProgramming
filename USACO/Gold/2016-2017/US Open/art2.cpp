ac7d63328cf26a54441b9e7384de96e653d5862a
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-11-03 11:20:58 -0700

    Update art2.cpp

--- USACO/Gold/2016-2017/US Open/art2.cpp
+++ USACO/Gold/2016-2017/US Open/art2.cpp
@@ -1,66 +1,62 @@
-#include <iostream>
-#include <stack>
 #include <vector>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <set>
 #include <map>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
-const int INF = 10000000;
-int solve(vector<int> v){
-    stack<int> s;
-    int n = v.size();
-    map<int,int> oc, noc;
-    for(int i = 0; i < n; i++){
-        //cin >> v[i];
-        oc[v[i]]++;
-    }
-    int myMax = 0;
-    for(int i = 0; i < n; i++){
-        noc[v[i]]++;
-        if(oc[v[i]] == 1){
-            myMax = max(myMax, (int)s.size() + 1);
-        }else if(oc[v[i]] == noc[v[i]]){
-            //cout << s.size() << endl;
-            if(s.size() == 0 || s.top() != v[i]){
-                return INF;
-            }
-            s.pop();
-        }else if(noc[v[i]] == 1){
-            s.push(v[i]);
-        }
-        myMax = max(myMax, (int)s.size());
-    }
-    return myMax;
-}
-int main(){
+int main() {
     freopen("art2.in", "r", stdin);
     freopen("art2.out", "w", stdout);
-    //cout << "HELLO" << endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin >> n;
-    stack<int> s;
-    vector<int> v(n);
-    vector<vector<int>> vec;
-    for(int i = 0; i < n; i++){
-        cin >> v[i];
-        if(v[i] == 0){
-            vec.push_back({});
-        }else{
-            if(vec.size() == 0){
-                vec.resize(1), vec[0].push_back(v[i]);
-                continue;
-            }
-            vec[vec.size() - 1].push_back(v[i]);
+    vector<long long> arr(n);
+    long long myMin[n + 1];
+    long long myMax[n + 1];
+    for (long long i = 0; i <= n; i++) {
+        myMin[i] = (long long)1e8;
+        myMax[i] = -1;
+    }
+    set<long long> s;
+    for (long long i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] == 0) {
+            continue;
         }
+        s.insert(arr[i]);
+        myMin[arr[i]] = min(myMin[arr[i]], i);
+        myMax[arr[i]] = max(myMax[arr[i]], i);
     }
-    //return 0;
-    int ans = 0;
-    for(vector<int> vv: vec){
-        int x = solve(vv);
-        //cout << x << endl;
-        ans = max(ans, x);
+    int interval[2 * n + 3];
+    for (long long i = 0; i <= 2 * n + 2; i++) {
+        interval[i] = 0;
     }
-    if(ans == INF){
-        cout << -1 << endl;
-        return 0;
+    for (long long i: s) {
+        interval[myMin[i] * 2]++;
+        interval[myMax[i] * 2 + 1]--;
     }
-    cout << ans << endl;
+    long long a = 0;
+    long long ans = 0;
+    for (long long i = 0; i <= 2 * n + 2; i++) {
+        ans += interval[i];
+        interval[i] = ans;
+        a = max(a, ans);
+    }
+    for (int i: s) {
+        if (myMin[i] == myMax[i]) {
+            continue;
+        }
+        if (interval[2 * myMin[i]] - 1 != interval[2 * myMax[i] + 1]) {
+            cout << -1;
+            return 0;
+        }
+        //cout << interval[2 * myMin[i]] << " " << interval[2 * myMax[i] + 1] << endl;
+    }
+    cout << a;
 }
