2a219304f28b20baabc13338d3e5e0f0ae155776
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-11-08 07:34:30 -0800

    Create dynamic_range_sum_queries.cpp

---
+++ USACO Guide/Gold/Data Structures/Point Update Range Sum/dynamic_range_sum_queries.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <map>
+#include <set>
+ 
+#define ll long long
+using namespace std;
+struct segmentTree {
+    vector<ll> v;
+    vector<ll> val;
+ 
+    ll ID = 0;
+ 
+    ll op(ll a, ll b) {
+        return a + b;
+    }
+ 
+    ll query(int dum, int tl, int tr, int& l, int& r) {
+        if (tr < l || tl > r) {
+            return ID;
+        }
+        if (tl >= l && tr <= r) {
+            return val[dum];
+        }
+        ll mid = (tl + tr) >> 1;
+        dum = dum << 1;
+        return query(dum, tl, mid, l, r) + query(dum + 1, mid + 1, tr, l, r);
+    }
+ 
+    ll query(int l, int r) {
+        return query(1, 0, (int)v.size() - 1, l, r);
+    }
+ 
+    void update(int x, ll y) {
+        int cur = (int) v.size() + x;
+        int curX = x;
+        int curY = x;
+        while (true) {
+            val[cur] += y;
+            if (cur == 0) {
+                break;
+            }
+            if(cur % 2 == 0) {
+                curY = 2 * curY - curX + 1;
+            } else {
+                curX = 2 * curX - curY - 1;
+            }
+            cur /= 2;
+        }
+    }
+ 
+    void resz(int n) {
+        v.resize((1 << (int) ceil(log2(n))));
+        val.resize(v.size() * 2);
+    }
+ 
+};
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    segmentTree st;
+    int n, q;
+    cin >> n >> q;
+    st.resz(n + 5);
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        st.update(i, v[i]);
+    }
+    while(q--) {
+        int dum;
+        cin >> dum;
+        int x, y;
+        cin >> x >> y;
+        if (dum == 1) {
+            x--;
+            st.update(x, y - st.query(x, x));
+        } else {
+            cout << st.query(x - 1, y - 1) << '\n';
+        }
+    }
+}
