e100f1f150e3a06a719fb4ac1eef1335c26b8e2b
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-11-09 08:44:06 -0800

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,119 @@
+#include <cmath>
+#include <iostream>
+#include <set>
+#include <climits>
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iomanip>
+#include <unordered_map>
+#include <type_traits>
+#include <string>
+#include <queue>
+#include <map>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+
+using namespace std;
+#define ll long long
+struct segmentTree {
+    vector<ll> v;
+    vector<ll> val;
+
+    ll ID = (int)1e8;
+
+    ll op(ll a, ll b) {
+        return min(a,b);
+    }
+
+    ll query(int dum, int tl, int tr, int& l, int& r) {
+        if (tr < l || tl > r) {
+            return ID;
+        }
+        if (tl >= l && tr <= r) {
+            return val[dum];
+        }
+        ll mid = (tl + tr) >> 1;
+        dum = dum << 1;
+        return op(query(dum, tl, mid, l, r), query(dum + 1, mid + 1, tr, l, r));
+    }
+
+    ll query(int l, int r) {
+        return query(1, 0, (int)v.size() - 1, l, r);
+    }
+
+    void update(int x, ll y) {
+        int cur = (int) v.size() + x;
+        int curX = x;
+        int curY = x;
+        while (true) {
+            val[cur] = op(op(query(curX, x - 1), query(x + 1, curY)), y);
+            if (cur == 0) {
+                break;
+            }
+            if(cur % 2 == 0) {
+                curY = 2 * curY - curX + 1;
+            } else {
+                curX = 2 * curX - curY - 1;
+            }
+            cur /= 2;
+        }
+    }
+
+    void resz(int n) {
+        v.resize((1 << (int) ceil(log2(n))));
+        val.resize(v.size() * 2);
+    }
+
+};
+int conv (char c) {
+    if (c == '(') return 1;
+    else return -1;
+}
+int main() {
+    freopen("cbs.in", "r", stdin);
+    freopen("cbs.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int K, N;
+    cin >> K >> N;
+    segmentTree st[K];
+    vector<string> s(K);
+    for (int i = 0; i < K; i++) {
+        cin >> s[i];
+        vector<int> depth = {0};
+        st[i].resz(s[i].length() + 1);
+        st[i].update(0, 0);
+        for (int j = 0; j < s[i].length(); j++) {
+            depth.push_back(depth.back() + conv(s[i][j]));
+            st[i].update(j + 1, depth.back());
+        }
+    }
+    //cout << st[0].query(11, 14) << endl;
+    //cout << st[0].query(10, 10) << endl;
+    map<vector<int>,vector<int>> myMap;
+    long long cntr = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        //first element
+        //starting point is at depth[i + 1], at point i
+        vector<int> depths;
+        for (int k = 0; k < K; k++) {
+            depths.push_back(st[k].query(i + 1, i + 1)); //at point i
+        }
+        for (int ind: myMap[depths]) {
+            //cout << i + 1 << " " << ind << endl;
+            bool fine = true;
+            for (int j = 0; j < K; j++) {
+                if (st[j].query(i + 2, ind + 1) != st[j].query(i + 1, i + 1)) {
+                    fine = false;
+                }
+            }
+            if (fine) {
+                cntr++;
+            }
+        }
+        myMap[depths].push_back(i);
+    }
+    cout << cntr << endl;
+}
