86d44819f5005bf49f0cf9ec51c2a1f1009dce7d
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-11-11 20:00:57 -0800

    Update α.cpp

--- α.cpp
+++ α.cpp
@@ -22,23 +22,25 @@ struct Coord {
     }
 };
 int main() {
+    freopen("marathon.in", "r", stdin);
+    freopen("marathon.in", "w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int N, Q;
     cin >> N >> Q;
     vector<Coord> vec(N);
+    vector<int> gain(N); //how much will we gain by skipping this interval (maximal lost cost)
     for (int i = 0; i < N; i++) {
         cin >> vec[i].x >> vec[i].y;
+        gain[i] = 0;
     }
     vector<int> dist(N + 1); //distance if we use the first X courses
     dist[0] = dist[1] = 0;
     for (int i = 2; i <= N; i++) {
         dist[i] = dist[i - 1] + (vec[i - 1].dist(vec[i - 2]));
-        //cout << i << " " << dist[i] << '\n';
     }
-    //cout << '\n';
-    for (int i = 0; i < N; i++) {
-        cout << i << " " << dist[i] << '\n';
+    for (int i = 1; i < N; i++) {
+        gain[i] = vec[i - 1].dist(vec[i + 1]) - vec[i - 1].dist(vec[i]) - vec[i].dist(vec[i + 1]);
     }
     while (Q--) {
         char c;
@@ -46,7 +48,13 @@ int main() {
         if (c == 'Q') {
             int i, j;
             cin >> i >> j;
-            cout << dist[j] - dist[i] << '\n';
+            int base = dist[j] - dist[i];
+            i--, j--;
+            int myMin = 0;
+            for (int k = i + 1; k <= j - 1; k++) {
+                myMin = min(myMin, gain[k]);
+            }
+            cout << base + myMin << '\n';
         } else {
             int ind, x, y;
             cin >> ind >> x >> y;
@@ -64,13 +72,10 @@ int main() {
                 for (int i = ind + 2; i <= N; i++) {
                     dist[i] += vec[ind + 1].dist({x, y}) - vec[ind + 1].dist(vec[ind]);
                 }
-                /*
-                cout << endl;
-                for (int j = 0; j <= N; j++) {
-                    cout << j << " " << dist[j] << '\n';
-                }
-                cout << endl;
-                 */
+                vec[ind] = {x, y};
+                if (ind >= 2) gain[ind - 1] = vec[ind - 2].dist(vec[ind]) - vec[ind - 1].dist(vec[ind]) - vec[ind - 1].dist(vec[ind - 2]);
+                if (ind + 1 < gain.size()) gain[ind] = vec[ind - 1].dist(vec[ind + 1]) - vec[ind].dist(vec[ind - 1]) - vec[ind].dist(vec[ind + 1]);
+                if (ind + 2 < gain.size()) gain[ind + 1] = vec[ind + 2].dist(vec[ind]) - vec[ind + 1].dist(vec[ind]) - vec[ind + 2].dist(vec[ind + 1]);
             }
         }
     }
