9b388ee113a97b60f73358873825f8dd467a36e1
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2021-11-29 16:21:23 -0800

    Update 907D.cpp

--- Codeforces/907D.cpp
+++ Codeforces/907D.cpp
@@ -26,25 +26,12 @@ tree<
 set<pair<int, int>> generate_edges(vector<int> v, int n, int m) {
     set<pair<int, int>> s;
     for (int i = 0; i < v.size(); i++) {
-        if (i - 1 >= 0 && (i - 1) % m != (m -1 ) % m) s.insert({v[i], v[i - 1]});
+        if (i - 1 >= 0 && (i - 1) % m != (m - 1) % m) s.insert({v[i], v[i - 1]});
         if (i + 1 < v.size() && (i + 1) % m != 0) s.insert({v[i], v[i + 1]});
         if (i - m >= 0) s.insert({v[i], v[i - m]});
         if (i + m < v.size()) s.insert({v[i], v[i + m]});
     }
     return s;
-}
-
-bool valid(vector<int> v, int n, int m) {
-    assert(v.size() == n * m);
-    vector<int> dum;
-    for (int i = 0; i < n * m; i++) {
-        dum.push_back(i);
-    }
-    set<pair<int, int>> s1 = generate_edges(dum, n, m);
-    set<pair<int, int>> s2 = generate_edges(v, n, m);
-    vector<pair<int, int>> vec;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), std::back_inserter(vec));
-    return vec.empty();
 }
 
 vector<int> f(int n, int m) {
@@ -66,7 +53,7 @@ vector<int> f(int n, int m) {
             int x = *s.find_by_order(r);
             bool valid = true;
             ans.push_back(x);
-            if (ans.size() >= 2 && (ans.size() - 2) % m != (m - 1) % m && s1.count({ans.back(), ans[ans.size() - 2]})) {
+            if (ans.size() >= 2 && ans.size() % m != 1 % m && s1.count({ans.back(), ans[ans.size() - 2]})) {
                 valid = false;
             }
             if (ans.size() >= m + 1 && s1.count({ans.back(), ans[ans.size() - m - 1]})) {
@@ -88,8 +75,8 @@ int main() {
     cin.tie(NULL);
     int n, m;
     cin >> n >> m;
-    int t = 100;
-    while(t--) {
+    int t = 10000;
+    while (t--) {
         vector<int> v = f(n, m);
         if (v.size() == n * m) {
             cout << "YES\n";
