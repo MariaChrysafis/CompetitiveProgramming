2890649254b5e7a101105ca2631f9d3e2cfe9e4e
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2021-12-11 10:10:07 -0800

    Create 1608A.cpp

---
+++ Codeforces/1608A.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <vector>
+
+#define ll long long
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cout << i + 2 << ' ';
+        }
+        cout << '\n';
+    }
+}
