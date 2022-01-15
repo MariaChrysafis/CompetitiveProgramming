01e372d5b2a2ff896d830dbfe98865549ec32a12
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-01-15 12:50:16 -0800

    Create abc235_b.cpp

---
+++ AtCoder/abc235_b.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <set>
+#include <climits>
+#include <cmath>
+#include <cassert>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n; i++) {
+        if (arr[i] <= arr[i - 1]) {
+            cout << arr[i - 1];
+            return 0;
+        }
+    }
+    cout << arr[n - 1];
+}
