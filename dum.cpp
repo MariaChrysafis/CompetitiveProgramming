dbcd3929488b78df67b0258e7351a450b254b689
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-01-31 16:59:47 -0800

    Update dum.cpp

--- dum.cpp
+++ dum.cpp
@@ -14,69 +14,44 @@
 
 using namespace std;
 typedef int64_t ll;
-struct segmentTree {
-    vector<ll> v;
-    vector<ll> val;
-
-    ll ID = 1e9;
-
-    ll op(ll a, ll b) {
-        return min(a,b);
+class RMQ {
+public:
+    vector<vector<int>> dp;
+    int query (int l, int r) {
+//        cout << l << " " << r << '\n';
+        int sz = log2(r - l + 1);
+        int ans = min(dp[l][sz], dp[r - (1 << sz) + 1][sz]);
+//        cout << l << " " << r << '\n';
+        return ans;
     }
-
-    ll query(int dum, int tl, int tr, int& l, int& r) {
-        if (tr < l || tl > r) {
-            return ID;
+    void update (vector<int> arr) {
+        dp.resize(arr.size());
+        for (int i = 0; i < arr.size(); i++) {
+            dp[i].resize(32);
         }
-        if (tl >= l && tr <= r) {
-            return val[dum];
-        }
-        ll mid = (tl + tr) >> 1;
-        dum = dum << 1;
-        return op(query(dum, tl, mid, l, r), query(dum + 1, mid + 1, tr, l, r));
-    }
-
-    ll query(int l, int r) {
-        return query(1, 0, (int)v.size() - 1, l, r);
-    }
-
-    void update(int x, ll y) {
-        int cur = (int) v.size() + x;
-        int curX = x;
-        int curY = x;
-        while (true) {
-            val[cur] = op(op(query(curX, x - 1), query(x + 1, curY)), y);
-            if (cur == 0) {
-                break;
+        for (int j = 0; j <= 31; j++) {
+            for (int i = 0; i < arr.size(); i++) {
+                if (j == 0) {
+                    dp[i][j] = arr[i];
+                    continue;
+                }
+                dp[i][j] = min(dp[min(i + (1 << (j - 1)), (int)arr.size() - 1)][j - 1], dp[i][j - 1]);
             }
-            if(cur % 2 == 0) {
-                curY = 2 * curY - curX + 1;
-            } else {
-                curX = 2 * curX - curY - 1;
-            }
-            cur /= 2;
         }
     }
-
-    void resz(int n) {
-        v.resize((1 << (int) ceil(log2(n))));
-        val.resize(v.size() * 2);
-    }
-
 };
+RMQ rmq;
 int l = 0;
 int r = -1;
 int sq = -1;
 int ans = 0;
 vector<set<int>> oc;
 vector<int> arr;
-segmentTree st;
 bool comp (pair<pair<int,int>,int> p1, pair<pair<int,int>,int> p2) {
     assert(sq != -1);
     return ((pair<int,int>){p1.first.first/sq, p1.first.second} < (pair<int,int>){p2.first.first/sq, p2.first.second});
 }
 int prevOc (set<int>& s, int ind) {
-//    cout << "INDEX " << ind << '\n';
     assert(s.find(ind) != s.end());
     auto it = s.lower_bound(ind);
     if (it == s.begin()) return -1;
@@ -84,13 +59,11 @@ int prevOc (set<int>& s, int ind) {
     return *it;
 }
 int nxtOc (set<int>& s, int ind) {
-//    assert(s.find(ind) != s.end());
     auto it = s.upper_bound(ind);
     if (it == s.end()) return -1;
     return *it;
 }
 void addRight (int ind) {
-//    cout << "ADD RIGHT " << l << " " << r << '\n';
     assert(ind == r + 1);
     r++;
     if (prevOc(oc[arr[ind]], ind) < l) {
@@ -98,13 +71,12 @@ void addRight (int ind) {
         return;
     }
     int i = prevOc(oc[arr[ind]], ind);
-    if (st.query(i, ind) == arr[i]) {
+    if (rmq.query(i, ind) == arr[i]) {
         return;
     }
     ans++;
 }
 void removeRight (int ind) {
-//    cout << "REMOVE RIGHT " << l << " " << r << '\n';
     assert(ind == r);
     r--;
     if (prevOc(oc[arr[ind]], ind) < l) {
@@ -112,49 +84,48 @@ void removeRight (int ind) {
         return;
     }
     int i = prevOc(oc[arr[ind]], ind);
-    if (st.query(i, ind) == arr[i]) {
+    if (rmq.query(i, ind) == arr[i]) {
         return;
     }
     ans--;
 }
 void removeLeft (int ind) {
-//    cout << "REMOVE LEFT " << l << " " << r << '\n';
     assert(ind == l);
     l++;
-    if (nxtOc(oc[arr[ind]], ind) > r) {
+    if (nxtOc(oc[arr[ind]], ind) > r || nxtOc(oc[arr[ind]], ind) == -1) {
         ans--;
         return;
     }
     int i = nxtOc(oc[arr[ind]], ind);
-    if (st.query(ind, i) == arr[i]) {
+    if (rmq.query(ind, i) == arr[i]) {
         return;
     }
     ans--;
 }
 void addLeft (int ind) {
-//    cout << "ADD LEFT " << l << " " << r << '\n';
     assert(ind == l - 1);
     l--;
-    if (nxtOc(oc[arr[ind]], ind) > r) {
+    if (nxtOc(oc[arr[ind]], ind) > r || nxtOc(oc[arr[ind]], ind) == -1) {
         ans++;
         return;
     }
     int i = nxtOc(oc[arr[ind]], ind);
-    if (st.query(ind, i) == arr[i]) {
+    if (rmq.query(ind, i) == arr[i]) {
         return;
     }
     ans++;
 }
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int N, Q;
     cin >> N >> Q;
-    st.resz(N);
     oc.resize(N), arr.resize(N);
     for (int i = 0; i < N; i++) {
         cin >> arr[i]; arr[i]--;
         oc[arr[i]].insert(i);
-        st.update(i, arr[i]);
     }
+    rmq.update(arr);
     sq = sqrt(N);
     vector<pair<pair<int,int>,int>> vec(Q);
     for (int i = 0; i < Q; i++) {
