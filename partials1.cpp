6f200c7de1053b12cf564e8985dee2b81440c42b
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-02-03 21:17:23 -0800

    Update partials1.cpp

--- partials1.cpp
+++ partials1.cpp
@@ -62,22 +62,26 @@ vector<pair<ll,ll>> get_hash (string s) {
 	return v;
 }
 
+vector<ll> powr1, powr2;
+vector<ll> ipowr1, ipowr2;
+
 set<pair<ll,ll>> get_substring_hash (string s) {
 	set<pair<ll,ll>> mySet;
 	pair<ll,ll> st = get_hash(s).back();
 	for (int i = 0; i < s.length(); i++) {
 		pair<ll,ll> cur = st;
-		//cout << cur.first << " " << cur.second << '\n';
 		mySet.insert(cur);
 		for (int j = s.length() - 1; j >= i; j--) {
-			//cout << cur.first << " " << cur.second << '\n';
 			cur.first -= (s[j] - 'a' + 1), cur.second -= (s[j] - 'a' + 1);
-			cur.first = mult(cur.first, inv(base, MOD1), MOD1);
-			cur.second = mult(cur.second, inv(base, MOD2), MOD2);
+			cur.first = mult(cur.first, ipowr1[1], MOD1);
+			cur.second = mult(cur.second, ipowr2[1], MOD2);
+			if (mySet.count(cur)) {
+				break;
+			}
 			mySet.insert(cur);
 		}
-		st.first -= mult(s[i] - 'a' + 1, binPow(base, s.length() - i - 1, MOD1), MOD1);
-		st.second -= mult(s[i] - 'a' + 1, binPow(base, s.length() - i - 1, MOD2), MOD2);
+		st.first -= mult(s[i] - 'a' + 1, powr1[s.length() - i - 1], MOD1);
+		st.second -= mult(s[i] - 'a' + 1, powr2[s.length() - i - 1], MOD2);
 		st.first += MOD1, st.second += MOD2, st.first %= MOD1, st.second %= MOD2;
 	}
 	return mySet;
@@ -90,20 +94,29 @@ int main() {
     cin.tie(NULL);
     int N;
     cin >> N;
+    powr1 = {1}, powr2 = {1};
+    while (powr1.size() != N + 10) {
+    	powr1.push_back(mult(powr1.back(), base, MOD1));
+    	powr2.push_back(mult(powr2.back(), base, MOD2));
+    }
+    for (ll x: powr1) ipowr1.push_back(inv(x, MOD1));
+    for (ll x: powr2) ipowr2.push_back(inv(x, MOD2));
     vector<string> arr;
     arr.resize(N);
     for (int i = 0; i < N; i++) {
     	cin >> arr[i];
     }
     map<pair<ll,ll>,ll> oc;
+    vector<set<pair<ll,ll>>> tot;
     for (int i = 0; i < arr.size(); i++) {
-    	for (pair<ll,ll> x: get_substring_hash(arr[i])) {
+    	tot.emplace_back(get_substring_hash(arr[i]));
+    	for (pair<ll,ll> x: tot.back()) {
     		oc[x]++;
     	}
     }
     for (int i = 0; i < N; i++) {
     	int ans = 0;
-    	for (pair<ll,ll> x: get_substring_hash(arr[i])) {
+    	for (pair<ll,ll> x: tot[i]) {
     		if (oc[x] == 1) {
     			ans++;
     		}
