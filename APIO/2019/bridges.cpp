5a26dfb5901bfab73335e41ae6db0302ac4f38cf
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-02-18 19:47:07 -0800

    Update bridges.cpp

--- APIO/2019/bridges.cpp
+++ APIO/2019/bridges.cpp
@@ -11,21 +11,21 @@
 #include <queue>
 #include <map>
 #include <stack>
+//
 #pragma GCC target ("avx2")
 #pragma GCC optimization ("O3")
 #pragma GCC optimization ("unroll-loops")
 #pragma GCC optimization ("Ofast")
- 
+
 using namespace std;
- 
-class Updates {
-public:
+
+struct Updates{
     int parentInd;
     int prev_parent;
     int compInd;
     int prev_comp;
 };
- 
+
 class DisjointSetUnion {
 private:
     vector<int> parent;
@@ -38,11 +38,11 @@ public:
     int getConnectedComponents() const {
         return connectedComponents;
     }
- 
+
     int getNeighbors(int x) {
         return compSize[find_head(x)];
     }
- 
+
 public:
     void resz(int sz) {
         n = sz;
@@ -52,7 +52,7 @@ public:
             parent[i] = i, compSize[i] = 1;
         }
     }
- 
+
     int find_head(int x) const {
         int cur = x;
         while (cur != parent[cur]) {
@@ -60,8 +60,8 @@ public:
         }
         return cur;
     }
- 
-    void join(int x, int y, bool b) {
+
+    void join0(int x, int y) {
         x = find_head(x);
         y = find_head(y);
         if (x == y) {
@@ -70,12 +70,26 @@ public:
         if (compSize[x] > compSize[y]) {
             swap(x, y);
         }
-        if (b) myStack.push({x, parent[x], y, compSize[y]}), cntr++;
         parent[x] = y;
         compSize[y] += compSize[x];
         connectedComponents--;
     }
- 
+
+    void join1(int x, int y) {
+        x = find_head(x);
+        y = find_head(y);
+        if (x == y) {
+            return;
+        }
+        if (compSize[x] > compSize[y]) {
+            swap(x, y);
+        }
+        myStack.push({x, parent[x], y, compSize[y]}), cntr++;
+        parent[x] = y;
+        compSize[y] += compSize[x];
+        connectedComponents--;
+    }
+
     void revert(int x) {
         while (x--) {
             Updates u = myStack.top();
@@ -85,23 +99,23 @@ public:
             connectedComponents++;
         }
     }
- 
+
     bool comp(int x, int y) {
         return (find_head(x) == find_head(y));
     }
 };
- 
+
 class Query {
 public:
     int w;
     int x;
     int type;
     int q;
- 
+
     Query(int w, int x, int type, int q) {
         this->w = w, this->x = x, this->type = type, this->q = q;
     }
- 
+
     bool operator<(const Query myQuery) const {
         if (myQuery.w != w) return (myQuery.w < w);
         if (myQuery.x != x) return (myQuery.x < x);
@@ -109,19 +123,24 @@ public:
         return false;
     }
 };
- 
+
 bool comp_index(Query a, Query b) {
     return (a.q < b.q);
 }
- 
+
 void compress(vector<pair<pair<int, int>, int>> &edges, vector<vector<Query>> &blocks) {
-    set<int> w;
-    for (auto &p: edges) w.insert(p.second);
-    for (auto &v: blocks) for (auto &q: v) w.insert(q.w);
-    map<int, int> myMap;
+    vector<int> w;
+    for (auto &p: edges) w.push_back(p.second);
+    for (auto &v: blocks) for (auto &q: v) w.push_back(q.w);
+    sort(w.begin(), w.end());
+    int prev = -1;
     int cntr = 0;
-    for (int i: w) {
-        myMap[i] = cntr++;
+    map<int,int> myMap;
+    for (int i = 0; i < w.size(); i++) {
+        if (w[i] != prev) {
+            prev = w[i];
+            myMap[w[i]] = cntr++;
+        }
     }
     for (int i = 0; i < edges.size(); i++) {
         edges[i].second = myMap[edges[i].second];
@@ -132,7 +151,7 @@ void compress(vector<pair<pair<int, int>, int>> &edges, vector<vector<Query>> &b
         }
     }
 }
- 
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -143,18 +162,19 @@ int main() {
         int x, y, z;
         cin >> x >> y >> z;
         x--, y--;
-        edges.push_back({{x, y}, z});
+        edges.emplace_back(make_pair(x, y), z);
     }
     int Q;
     cin >> Q;
     vector<vector<Query>> blocks;
     blocks.emplace_back();
     for (int i = 0; i < Q; i++) {
-        if (blocks.back().size() > 1000) {
+        if (blocks.back().size() > 1250) {
             blocks.emplace_back();
         }
         int t, w, x;
         cin >> t >> x >> w;
+        t--;
         x--;
         blocks.back().emplace_back(Query(w, x, t, i));
     }
@@ -174,17 +194,17 @@ int main() {
             cnt[i] = 0;
         }
         for (Query& q: v) {
-            if (q.type == 1) {
+            if (!q.type) {
                 uncertain.push_back(q.x);
                 cnt[q.x]++;
             }
         }
-        stack<pair<int,int>> myStack;
+        vector<pair<int,int>> myStack;
         vector<int> mnt[v.size()];
         for (int i = 0; i < v.size(); i++) {
-            if (v[i].type == 1) {
+            if (!v[i].type) {
                 default_edges[v[i].x] = v[i].w;
-                myStack.push({v[i].x, v[i].w});
+                myStack.emplace_back(v[i].x, v[i].w);
             } else {
                 for (int j: uncertain) {
                     if (default_edges[j] >= v[i].w) {
@@ -193,9 +213,8 @@ int main() {
                 }
             }
         }
-        while (!myStack.empty()) {
-            default_edges[myStack.top().first] = myStack.top().second;
-            myStack.pop();
+        for (int i = myStack.size() - 1; i >= 0; i--) {
+            default_edges[myStack[i].first] = myStack[i].second;
         }
         vector<int> e[M + Q];
         for (int i = 0; i < M; i++) {
@@ -207,31 +226,31 @@ int main() {
         int prev = M + Q;
         for (int ind = 0; ind < v.size(); ind++) {
             Query q = v[ind];
-            if (q.type == 1) {
+            if (!q.type) {
                 continue;
             }
             for (int i = q.w; i < prev; i++) {
                 for (int j: e[i]) {
-                    dsu.join(edges[j].first.first, edges[j].first.second, 0);
+                    dsu.join0(edges[j].first.first, edges[j].first.second);
                 }
             }
             prev = q.w;
             dsu.cntr = 0;
-            for (auto& x: mnt[v[ind].q - total]) {
-                dsu.join(edges[x].first.first, edges[x].first.second, 1);
+            for (int x: mnt[v[ind].q - total]) {
+                dsu.join1(edges[x].first.first, edges[x].first.second);
             }
             myVec.emplace_back(q.q, dsu.getNeighbors(q.x));
             dsu.revert(dsu.cntr);
         }
         sort(v.begin(), v.end(), comp_index);
         for (Query &q: v) {
-            if (q.type == 1) {
+            if (!q.type) {
                 default_edges[q.x] = q.w;
             }
         }
         total += v.size();
     }
- 
+
     sort(myVec.begin(), myVec.end());
     for (auto &p: myVec) {
         cout << p.second << '\n';
