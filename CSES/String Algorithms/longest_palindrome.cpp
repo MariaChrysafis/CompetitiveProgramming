d3abd23d3adaf65272265e33dc98254aebbe0481
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-03-20 21:21:39 -0700

    Create longest_palindrome.cpp

---
+++ CSES/String Algorithms/longest_palindrome.cpp
@@ -0,0 +1,114 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <cmath>
+#include <map>
+#include <random>
+#include <cassert>
+#include <ctime>
+#include <cstdlib>
+#include <queue>
+#include <limits.h>
+using namespace std;
+ 
+int64_t binPow (int64_t x, int64_t y, int64_t MOD) {
+    x %= MOD;
+    int64_t res = x;
+    int64_t ans = 1;
+    while (y > 0) {
+        if (y & 1) {
+            ans *= res, ans %= MOD;
+        }
+        res *= res, res %= MOD;
+        y /= 2;
+    }
+    return ans;
+}
+int64_t inv (int64_t x, int64_t MOD) {
+    return binPow(x, MOD - 2, MOD);
+}
+vector<int64_t> powr = {1};
+vector<int64_t> ipowr = {1};
+struct Hasher {
+    void resz (int mod, int base, string str) {
+        int n = str.size();
+        this->sz = n;
+        this->MOD = mod, this->BASE = base;
+        this->pref.assign(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            pref[i] = (pref[i - 1] + (powr[n - i] * (str[i - 1] - 'a' + 1)) % MOD) % MOD;
+            pref[i] %= MOD;
+        }
+    }
+    int sz;
+    int MOD;
+    int BASE;
+    vector<int64_t> pref;
+    int64_t query (int l, int r) {
+        return (ipowr[sz - r - 1] * (pref[r + 1] - pref[l] + MOD) % MOD) % MOD;
+    }
+};
+Hasher h1;
+Hasher h2;
+string s1;
+bool isPalindrome (int l, int r) {
+    if (l < 0 || r >= s1.length()) return false;
+    return (h1.query(l, r) == h2.query((int)s1.length() - r - 1, (int)s1.length() - l - 1));
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (powr.size() != 1e6 + 5) {
+        powr.push_back(powr.back() * 31);
+        powr.back() %= (int)1e9 + 9;
+    }
+    ipowr.resize(powr.size());
+    ipowr[0] = 1; ipowr[1] = inv(powr[1], 1e9 + 9);
+    for (int i = 2; i < powr.size(); i++) {
+        ipowr[i] = (ipowr[i - 1] * ipowr[1]) % ((int)1e9 + 9);
+    }
+    cin >> s1;
+    h1.resz((int)1e9 + 9, 31, s1);
+    reverse(s1.begin(), s1.end());
+    h2.resz((int)1e9 + 9, 31, s1);
+    reverse(s1.begin(), s1.end());
+    pair<int,int> p = {0, 0};
+    for (int i = 0; i < s1.size(); i++) {
+        int l = 0;
+        int r = s1.size();
+        while (l != r) {
+            int m = (l + r + 1)/2;
+            if (isPalindrome(i - m, i + m)) {
+                l = m;
+            } else {
+                r = m - 1;
+            }
+        }
+        if (2 * l + 1 > p.second - p.first + 1) {
+            p = {i - l, i + l};
+        }
+    }
+    for (int i = 1; i < s1.size(); i++) {
+        if (s1[i - 1] != s1[i]) {
+            continue;
+        }
+        int l = 0;
+        int r = s1.size();
+        while (l != r) {
+            int m = (l + r + 1)/2;
+            if (isPalindrome(i - m - 1, i + m)) {
+                l = m;
+            } else {
+                r = m - 1;
+            }
+        }
+        if (2 * l + 2 > p.second - p.first + 1) {
+            p = {i - l - 1, i + l};
+        }
+        //p = best(p, {i - l - 1, i + l});
+    }
+    for (int i = p.first; i <= p.second; i++) {
+        cout << s1[i];
+    }
+}
