8c17701986264a6e4f20a8ac9e02010a6c86f5e6
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-03-22 18:38:14 -0700

    Update and rename range_xor_queries.cpp to Range Xor Queries.cpp

---
+++ CSES/Range Queries/Range Xor Queries.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <cmath>
+#include <map>
+#include <random>
+#include <cassert>
+#include <ctime>
+#include <cstdlib>
+#include <limits.h>
+ 
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+ 
+using namespace std;
+const int MOD = 1e9;
+ 
+template<class T>
+class SegmentTree {
+public:
+ 
+    SegmentTree (int N) {
+        N = (1 << ((int)floor(log2(N - 1)) + 1));
+        this->N = N;
+        val.assign(2 * N, ID);
+    }
+ 
+    void update (int x, T y) {
+        x += N - 1;
+        val[x] = y;
+        while (true) {
+            x = (x - 1)/2;
+            val[x] = merge(val[2 * x + 1], val[2 * x + 2]);
+            if (x == 0) {
+                break;
+            }
+        }
+    }
+ 
+    T query (int ind, int l, int r, int tl, int tr) {
+        if (tl >= l && tr <= r) {
+            return val[ind];
+        }
+        if (tr < l || tl > r) {
+            return ID;
+        }
+        return merge(query(2 * ind + 1, l, r, tl, (tl + tr)/2), query(2 * ind + 2, l, r, (tl + tr)/2 + 1, tr));
+    }
+ 
+    T query (int l, int r) {
+        return query(0, l, r, 0, N - 1);
+    }
+private:
+    vector<T> val;
+    T ID = 0;
+    T merge (T x, T y) {
+        return (x ^ y);
+    }
+    int N;
+};
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N, Q;
+    cin >> N >> Q;
+    SegmentTree<int64_t> st(N);
+    for (int i = 0; i < N; i++) {
+        int x; cin >> x;
+        st.update(i, x);
+    }
+    while (Q--) {
+        int x, y; cin >> x >> y;
+        x--, y--;
+        cout << st.query(x, y) << '\n';
+    }
+}
