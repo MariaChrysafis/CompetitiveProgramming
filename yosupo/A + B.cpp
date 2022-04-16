9a617f2aeb226f4ef131a34ae4f6da2e0d013a04
Maria Chrysafis (Olympia) <maria.chrysafis.junior@gmail.com>
2022-04-16 13:41:22 -0700

    Create A + B.cpp

---
+++ yosupo/A + B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <cassert>
+#include <map>
+#include <set>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+}
